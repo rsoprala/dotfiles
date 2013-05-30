652f336efa8b747e5c7c583c60a655f49fefca5c
Matt Havener <mhavener@yahoo-inc.com>
2013-05-30 15:31:36 +0000

    tmux

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ if [ ! -f $HOME/.gitconfig ]; then ln -s $PWD/gitconfig $HOME/.gitconfig; fi
 if [ ! -f $HOME/.screenrc ]; then ln -s $PWD/screenrc $HOME/.screenrc; fi
 if [ ! -d $HOME/.vim ]; then ln -s $PWD/vim $HOME/.vim; fi
 if [ ! -f $HOME/.vimrc ]; then ln -s $PWD/vimrc $HOME/.vimrc; fi
+if [ ! -f $HOME/.tmux.conf ]; then ln -s $PWD/tmux.conf $HOME/.tmux.conf; fi
 
 if [ ! -s $HOME/bin ]; then ln -s $PWD/bin $HOME/bin; fi
 
