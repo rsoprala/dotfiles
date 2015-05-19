998c90c2fc57f26bd9987becc183ca5a3ea0266e
rsoprala <rsoprala@yahoo-inc.com>
2015-05-19 14:20:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ git submodule update
 if [ $PWD != $HOME/dotfiles ]; then echo "Lazy matt only made this work if its checked out at $HOME/dotfiles"; fi
 if [ ! -d $HOME/.vim ]; then ln -s $PWD/vim $HOME/.vim; fi
 if [ ! -f $HOME/.vimrc ]; then ln -s $PWD/vimrc $HOME/.vimrc; fi
+if [ ! -f $HOME/.gitconfig ]; then ln -s $PWD/gitconfig $HOME/.gitconfig; fi
 if [ ! -f $HOME/.tmux.conf ]; then ln -s $PWD/tmux.conf $HOME/.tmux.conf; fi
 
 if [ ! -s $HOME/bin ]; then ln -s $PWD/bin $HOME/bin; fi
